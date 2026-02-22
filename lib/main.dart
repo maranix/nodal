9b75b777b09c4984160ff27ed4447c3580237a3a
maranix <ramanverma4183@gmail.com>
2026-02-22 12:59:39 +0530

    feat(sqlite3): switch to sqlite3 pub package
    * No need to create a custom implementation when a tested alternative is
      already available
    
    Better to save development + testing time and effort

--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,138 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:nodal/src/feature/profile/model/profile_model.dart';
-import 'package:nodal/src/feature/profile/repository/profile_repository.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:sqlite3/sqlite3.dart' as sqlite;
 
 void main() async {
-  runApp(const MainApp());
+  final db = sqlite.sqlite3.openInMemory();
+
+  db.execute(
+    "CREATE TABLE profiles (id INTEGER PRIMARY KEY, pos INTEGER NOT NULL);",
+  );
+
+  runApp(MainApp(db: db));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MainApp extends StatefulWidget {
+  const MainApp({super.key, required this.db});
 
-  static Future<List<Profile>> getCacheDirectoryProfiles() async {
-    final content = await getApplicationCacheDirectory();
-    final data = content.listSync();
+  final sqlite.Database db;
 
-    if (data.isEmpty) {
-      return [];
-    }
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
 
-    return data
-        .map((e) => Profile.fromJsonString(File(e.path).readAsStringSync()))
-        .toList();
+class _MainAppState extends State<MainApp> {
+  late final ValueNotifier<sqlite.ResultSet?> profileTableRowResult;
+
+  void deleteRowCallback(int id) {
+    widget.db.execute('DELETE FROM profiles WHERE id = $id');
+
+    profileTableRowResult.value = widget.db.select('SELECT * FROM profiles');
+  }
+
+  void addRowCallback() {
+    final pos = profileTableRowResult.value?.length ?? 0;
+
+    widget.db.execute('INSERT INTO profiles (pos) VALUES (?)', [pos + 1]);
+
+    profileTableRowResult.value = widget.db.select('SELECT * FROM profiles');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    profileTableRowResult = .new(null);
+  }
+
+  @override
+  void dispose() {
+    profileTableRowResult.dispose();
+    widget.db.close();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: FutureBuilder(
-          future: getCacheDirectoryProfiles(),
-          builder: (context, profileList) {
-            if (profileList.hasData) {
-              if (profileList.requireData.isEmpty) {
-                return Center(
-                  child: Text(
-                    "What The FUCk: ${profileList.requireData.length}",
-                  ),
-                );
-              }
-
-              return Center(
-                child: Column(
-                  spacing: 4,
-                  mainAxisAlignment: .center,
-                  children: profileList.requireData
-                      .map(
-                        (p) => ListTile(
-                          title: Text(p.uuid),
-                          trailing: IconButton.filled(
-                            onPressed: () async =>
-                                await ProfileRepository().delete(p),
-                            color: Colors.red,
-                            icon: Icon(Icons.delete),
-                          ),
-                        ),
-                      )
-                      .toList(),
-                ),
-              );
-            }
-            return Center(child: Text('Hello World!'));
-          },
+        body: Center(
+          child: RowList(
+            rowsNotifier: profileTableRowResult,
+            onDelete: (id) => deleteRowCallback(id),
+          ),
         ),
-        floatingActionButton: FloatingActionButton.small(
-          onPressed: () async {
-            final date = DateTime.now();
-
-            await ProfileRepository().create(
-              .defaults(firstName: date.toIso8601String(), dob: date),
-            );
-          },
-          child: Icon(Icons.add),
+        floatingActionButton: Column(
+          mainAxisAlignment: .end,
+          children: [AddRowButton(onPressed: addRowCallback)],
         ),
       ),
     );
+  }
+}
+
+class RowList extends StatelessWidget {
+  const RowList({
+    super.key,
+    required this.rowsNotifier,
+    required this.onDelete,
+  });
+
+  final ValueNotifier<sqlite.ResultSet?> rowsNotifier;
+  final void Function(int id) onDelete;
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: rowsNotifier,
+      builder: (context, rows, child) {
+        if (rows == null) {
+          return switch (child) {
+            null => Text('Nothing to be displayed here.'),
+            _ => child,
+          };
+        }
+
+        return ListView.builder(
+          itemCount: rows.length,
+          itemBuilder: (context, index) {
+            final row = rows[index];
+
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                spacing: 24,
+                mainAxisAlignment: .center,
+                children: [
+                  Text(row['pos'].toString(), textAlign: .center),
+                  DeleteRowButton(onPressed: () => onDelete(row['id'])),
+                ],
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
+
+class AddRowButton extends StatelessWidget {
+  const AddRowButton({super.key, required this.onPressed});
+
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton.filled(onPressed: onPressed, icon: Icon(Icons.add));
+  }
+}
+
+class DeleteRowButton extends StatelessWidget {
+  const DeleteRowButton({super.key, required this.onPressed});
+
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton.filled(onPressed: onPressed, icon: Icon(Icons.delete));
   }
 }
