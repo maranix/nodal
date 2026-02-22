d79ccfd94deee5bc59be70a65985d04b44021f7c
maranix <ramanverma4183@gmail.com>
2026-02-23 00:43:03 +0530

    refactor(button): move RawButton into core/widgets

--- lib/src/core/router/router.dart
+++ lib/src/core/router/router.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
+import 'package:nodal/src/core/widgets/widgets.dart';
 
 part 'router.g.dart';
 
@@ -95,64 +96,6 @@ final class ProfileSelectionRoute extends GoRouteData
               ),
               RawButton(onTap: () => context.pop(), label: 'Home'),
             ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class RawButton extends StatefulWidget {
-  final VoidCallback onTap;
-  final String label;
-  final Color color;
-
-  const RawButton({
-    super.key,
-    required this.onTap,
-    required this.label,
-    this.color = const Color(0xFF007AFF),
-  });
-
-  @override
-  State<RawButton> createState() => _RawButtonState();
-}
-
-class _RawButtonState extends State<RawButton> {
-  bool _isPressed = false;
-
-  @override
-  Widget build(BuildContext context) {
-    final Color textColor = switch (widget.color.computeLuminance()) {
-      > 0.5 => const Color(0xFF000000),
-      _ => const Color(0xFFFFFFFF),
-    };
-
-    return GestureDetector(
-      onTapDown: (_) => setState(() => _isPressed = true),
-      onTapUp: (_) => setState(() => _isPressed = false),
-      onTapCancel: () => setState(() => _isPressed = false),
-      onTap: widget.onTap,
-      child: Opacity(
-        opacity: _isPressed ? 0.8 : 1.0,
-        child: Transform.scale(
-          scale: _isPressed ? 0.98 : 1.0,
-          child: DecoratedBox(
-            decoration: BoxDecoration(
-              color: widget.color,
-              borderRadius: BorderRadius.circular(8),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-              child: Text(
-                widget.label,
-                style: TextStyle(
-                  color: textColor,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
           ),
         ),
       ),
